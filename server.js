9b5c1c86b8f9d55eebcddda38e24f1480ce65a52
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-08-20 17:31:28 +0530

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   return res.status(200).send({
